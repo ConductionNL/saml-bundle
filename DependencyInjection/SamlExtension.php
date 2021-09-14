08947c9914165248131a1bc8214443844636bcba
Robert Zondervan <Robert@conduction.nl>
2021-09-14 16:50:35 +0200

    Configuration (2)

--- DependencyInjection/SamlExtension.php
+++ DependencyInjection/SamlExtension.php
@@ -12,7 +12,22 @@ class SamlExtension extends Extension
 
     public function load(array $configs, ContainerBuilder $container)
     {
+        $this->loadConfig($configs, $container);
+
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+    }
+
+    private function loadConfig(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $processedConfiguration = $this->processConfiguration($configuration, $configs);
+
+        foreach ($processedConfiguration as $key => $value) {
+            $container->setParameter(
+                $this->getAlias().'.'.$key,
+                $value
+            );
+        }
     }
 }
