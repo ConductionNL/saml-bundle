a879c10f6775d56558f8c4e3f57b5142fa5f2b69
Robert Zondervan <Robert@conduction.nl>
2021-09-14 15:11:20 +0200

    geprut

--- DependencyInjection/SamlExtension.php
+++ DependencyInjection/SamlExtension.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class SamlBundleExtension extends Extension implements PrependExtensionInterface
+class SamlExtension extends Extension implements PrependExtensionInterface
 {
 
     public function load(array $configs, ContainerBuilder $container)
