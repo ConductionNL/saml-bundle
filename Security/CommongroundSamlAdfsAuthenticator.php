c2e62c3445afeb2b579302f8dcb8e8e2ab68546a
Robert Zondervan <Robert@conduction.nl>
2021-09-15 16:17:27 +0200

    Adfs Authenticator

---
+++ Security/CommongroundSamlAdfsAuthenticator.php
@@ -0,0 +1,170 @@
+<?php
+
+// src/Security/TokenAuthenticator.php
+
+/*
+ * This authenticator authenticates against DigiSpoof
+ *
+ */
+
+namespace App\Security;
+
+use Conduction\CommonGroundBundle\Security\User\CommongroundUser;
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Client;
+use OneLogin\Saml2\Auth;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+
+class CommongroundSamlAdfsAuthenticator extends AbstractGuardAuthenticator
+{
+    private $em;
+    private $params;
+    private $commonGroundService;
+    private $csrfTokenManager;
+    private $router;
+    private $urlGenerator;
+    private $session;
+    private Auth $samlAuth;
+    private XmlEncoder $xmlEncoder;
+
+    public function __construct(EntityManagerInterface $em, ParameterBagInterface $params, CommonGroundService $commonGroundService, CsrfTokenManagerInterface $csrfTokenManager, RouterInterface $router, UrlGeneratorInterface $urlGenerator, SessionInterface $session, Auth $samlAuth)
+    {
+        $this->em = $em;
+        $this->params = $params;
+        $this->commonGroundService = $commonGroundService;
+        $this->csrfTokenManager = $csrfTokenManager;
+        $this->router = $router;
+        $this->urlGenerator = $urlGenerator;
+        $this->session = $session;
+        $this->samlAuth = $samlAuth;
+        $this->xmlEncoder = new XmlEncoder(['xml_root_node_name' => 'md:EntityDescriptor']);
+    }
+
+    /**
+     * Called on every request to decide if this authenticator should be
+     * used for the request. Returning false will cause this authenticator
+     * to be skipped.
+     */
+    public function supports(Request $request)
+    {
+        return 'conduction_saml_artifact' === $request->attributes->get('_route')
+            && $request->isMethod('POST');
+    }
+
+    /**
+     * Called on every request. Return whatever credentials you want to
+     * be passed to getUser() as $credentials.
+     */
+    public function getCredentials(Request $request)
+    {
+        $credentials = [
+            'SAMLResponse'   => $request->request->get('SAMLResponse'),
+        ];
+
+        return $credentials;
+    }
+
+    public function samlResponseToUser(string $artifact): string
+    {
+        $result = base64_decode($artifact);
+
+        $data = $this->xmlEncoder->decode($result, 'xml');
+        $user = [];
+        foreach($data['Assertion']['AttributeStatement']['Attribute'] as $attribute)
+        {
+            switch($attribute['@Name']){
+                case 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name':
+                    $user['username'] = $attribute['AttributeValue'];
+                    break;
+                case 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname':
+                    $user['surname'] = $attribute['AttributeValue'];
+                    break;
+                case 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname':
+                    $user['givenName'] = $attribute['AttributeValue'];
+                    break;
+                case 'http://schemas.microsoft.com/identity/claims/displayname':
+                    $user['displayName'] = $attribute['AttributeValue'];
+                    break;
+                case 'http://schemas.microsoft.com/identity/claims/identityprovider':
+                    $user['identityProvider'] = $attribute['AttributeValue'];
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return end($nameIdExplode);
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $user = $this->samlResponseToUser($credentials['SAMLResponse']);
+
+        if (!isset($user['roles'])) {
+            $user['roles'] = [];
+        }
+
+        if (!in_array('ROLE_USER', $user['roles'])) {
+            $user['roles'][] = 'ROLE_USER';
+        }
+
+        array_push($user['roles'], 'scope.vrc.requests.read');
+        array_push($user['roles'], 'scope.orc.orders.read');
+        array_push($user['roles'], 'scope.cmc.messages.read');
+        array_push($user['roles'], 'scope.bc.invoices.read');
+        array_push($user['roles'], 'scope.arc.events.read');
+        array_push($user['roles'], 'scope.irc.assents.read');
+
+        return new CommongroundUser($user['username'], $user['identityProvider'], $user['displayName'], null, $user['roles'], $user['username'], null, 'person', false);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return new RedirectResponse($this->urlGenerator->generate('app_default_index'));
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        return new RedirectResponse($this->params->get('app_url').'/saml/Login');
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent.
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+
+    }
+
+    public function supportsRememberMe()
+    {
+        return true;
+    }
+
+    protected function getLoginUrl()
+    {
+        if ($this->params->get('app_subpath') && $this->params->get('app_subpath') != 'false') {
+            return '/'.$this->params->get('app_subpath').$this->router->generate('app_user_digispoof', [], UrlGeneratorInterface::RELATIVE_PATH);
+        } else {
+            return $this->router->generate('app_user_digispoof', [], UrlGeneratorInterface::RELATIVE_PATH);
+        }
+    }
+}
